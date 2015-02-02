d1609e84d0dda2bf178aa4f07e37ac5949751303
jinsong bai <bjstry@163.com>
2015-02-02 14:58:55 +0800

    1522-1458-update

--- php/common.php
+++ php/common.php
@@ -2,4 +2,12 @@
 	function bdate($a){
 		return date("Y-m-d H:i:s",$a);
 	}
+	function showlist($a=null){
+		if($a==1){
+			$sql = "select * from `gt_protables` where `htstatus`=1 and `xdstatus`=1 and `fhstatus`=1 and `kpstatus`=1 and `skstatus`=1 and `showstatus`=1";	
+		}else{
+			$sql = "select * from `gt_protables` where `showstatus`=1";
+		}
+		return mysql_query($sql);
+	}
 ?>
